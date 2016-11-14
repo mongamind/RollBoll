30ea0452462358475eeed5b05492a22474b5b429
Mon <mongamind@foxmail.com>
2016-11-15 02:11:13 +0800

    commit the roll ball project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public Camera cm;
+	public float speed;
+	public Text countText;
+
+	private Rigidbody rb;
+	private Vector3 offset;
+	private int count;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+		offset = cm.transform.position - transform.position;
+		count = 0;
+	}
+	
+	void FixedUpdate () {
+		float fHorAxis = Input.GetAxis ("Horizontal");
+		float fVerAxis = Input.GetAxis ("Vertical");
+		Vector3 force = new Vector3 (fHorAxis, 0, fVerAxis);
+		force = force * speed;
+		rb.AddForce (force);
+	}
+
+	void LateUpdate()
+	{
+		cm.transform.position = transform.position + offset;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Start")) {
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+	}
+
+	void setCountText()
+	{
+		countText.text = "Count: " + count;
+	}
+}
---
+++ Assets/Scripts/StartRoll.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartRoll : MonoBehaviour {
+
+	void Update () {
+		transform.Rotate (new Vector3 (45, 45, 45) * Time.deltaTime);
+	}
+}
